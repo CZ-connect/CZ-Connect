b0542851d37d55068753f7b0cce02064d3634871
Marijn <marijnvandenbos59@gmail.com>
2023-03-14 15:52:00 +0100

    centering

--- cz_app/lib/widget/store_input.dart
+++ cz_app/lib/widget/store_input.dart
@@ -10,46 +10,50 @@ class formWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: _formKey,
-        child: Column (
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'Enter your name',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your name';
-                }
-                return null;
-                },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'Enter your email',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty || !EmailValidator.validate(value)) {
-                  return 'The email address is invalid';
-                }
-                return null;
-              },
-            ),
-            const Padding(padding: EdgeInsets.all(8.0)),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ],
-        )
-        ),
+      child: Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your name',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                  validator: (String? value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        !EmailValidator.validate(value)) {
+                      return 'The email address is invalid';
+                    }
+                    return null;
+                  },
+                ),
+                const Padding(padding: EdgeInsets.all(8.0)),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
