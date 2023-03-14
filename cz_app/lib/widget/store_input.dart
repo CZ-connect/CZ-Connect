81ba24479aee6d38aa6a2caff7f0603d894ff31c
Marijn <marijnvandenbos59@gmail.com>
2023-03-14 15:34:34 +0100

    feature/form-input

---
+++ cz_app/lib/widget/store_input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+
+final _formKey = GlobalKey<FormState>();
+
+class formWidget extends StatelessWidget {
+  const formWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column (
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter your name',
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your name';
+                }
+                return null;
+                },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter your email',
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty || !EmailValidator.validate(value)) {
+                  return 'The email address is invalid';
+                }
+                return null;
+              },
+            ),
+            const Padding(padding: EdgeInsets.all(8.0)),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        )
+        ),
+    );
+  }
+}
