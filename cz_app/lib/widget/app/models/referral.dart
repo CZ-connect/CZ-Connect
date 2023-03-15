82550e9123a963bded363d56985376ca232a24e9
cadet <cadet@gunbuster.spacestation.local>
2023-03-15 10:40:10 +0100

    adding validator

--- cz_app/lib/widget/app/models/referral.dart
+++ cz_app/lib/widget/app/models/referral.dart
@@ -2,7 +2,8 @@ class Referral {
   int id;
   String status;
   String participantName;
+  String participantEmail;
   bool reimbursable;
 
-  Referral({required this.id, required this.status, required this.participantName, required this.reimbursable});
+  Referral({required this.id, required this.status, required this.participantName, required this.reimbursable, required this.participantEmail});
 }
