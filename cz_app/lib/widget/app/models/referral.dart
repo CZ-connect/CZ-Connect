321b9cd03b62b8bb4f2e220f6d236911edeb7175
cadet <cadet@gunbuster.spacestation.local>
2023-03-15 10:30:27 +0100

    first version of the referral model

---
+++ cz_app/lib/widget/app/models/referral.dart
@@ -0,0 +1,8 @@
+class Referral {
+  int id;
+  String status;
+  String participantName;
+  bool reimbursable;
+
+  Referral({required this.id, required this.status, required this.participantName, required this.reimbursable});
+}
