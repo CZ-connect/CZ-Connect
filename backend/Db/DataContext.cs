a4cd4f583f31a421ec310537ef6938ba43f70b42
JosVoogt2107 <j.voogt1@student.avans.nl>
2023-03-17 16:26:14 +0100

    add employee and edit refferal model

--- backend/Db/DataContext.cs
+++ backend/Db/DataContext.cs
@@ -8,4 +8,5 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
     public DbSet<Referral> Referrals { get; set; }
+    public DbSet<Employee> Employees { get; set; }
 }
---
+++ backend/Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace CZConnect.Models;
+
+public class Employee
+{
+    public string Id { get; set; }
+    public string EmployeeName { get; set; }
+}
