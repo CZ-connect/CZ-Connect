b120824cd1a7f517309894dc8c67831a4452e58f
JosVoogt2107 <j.voogt1@student.avans.nl>
2023-03-19 23:52:57 +0100

    opzet api voor refferals

--- backend/Controllers/ReferralController.cs
+++ backend/Controllers/ReferralController.cs
@@ -11,10 +11,19 @@ public class ReferralController : ControllerBase
 {
     private readonly DataContext _dataContext;
 
+    public Employee EmployeeExists(string id) 
+    {
+        return _dataContext.Employees.FirstOrDefault(e => e.Id == id);
+    }
+
     public ReferralController(DataContext dataContext) =>
         _dataContext = dataContext;
     
     [HttpGet]
     public async Task<List<Referral>> Get() =>
         await _dataContext.Referrals.ToListAsync();
+
+    [HttpGet("{id:string}")]
+    public async Task<List<Referral>> GetEmployeeRefferals(string id) =>
+        await _dataContext.Referrals.Where(e => e.EmployeeId == id).ToListAsync();      
 }
--- backend/Models/Referral.cs
+++ backend/Models/Referral.cs
@@ -5,8 +5,8 @@ public class Referral
     public string Id { get; set; }
     public string ParticipantName { get; set; }
     public string ParticipantEmail { get; set; }
-    public string ParticipantEmail { get; set; }
     public string Status { get; set; }
     public DateTime RegistrationDate { get; set; }
+    public string EmployeeId { get; set; }
     public Employee Employee { get; set; }
 }
