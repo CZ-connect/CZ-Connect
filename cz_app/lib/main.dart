a8700004e9fd2326750d0dd8da0f00280dc23cfc
wballeko <w.vanballekom@student.avans.nl>
2023-03-22 13:01:29 +0100

    /dashboard route added

--- cz_app/lib/main.dart
+++ cz_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cz_app/widget/Dashboard/mainDashboard.dart';
 import 'package:cz_app/widget/app/form-app//appBackground.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CZ_connect',
+      initialRoute: '/',
+      routes: {
+        '/dashboard': (context) => const OverViewWidget(),
+      },
       home: Scaffold(
         body: backgroundWidget(),
       ),
