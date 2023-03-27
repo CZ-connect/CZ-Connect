033ec67d3c6a20b6b2563217fb51839d9a6bbd64
JosVoogt2107 <j.voogt1@student.avans.nl>
2023-03-27 17:07:28 +0200

    Headers toegevoegd aan Request

--- cz_app/lib/widget/app/services/referralService.dart
+++ cz_app/lib/widget/app/services/referralService.dart
@@ -4,13 +4,16 @@ import 'package:http/http.dart';
 
 class ReferralService {
   int userId;
-  //List<Referral> referrals;
   List<Referral> referrals = [];
 
   ReferralService({required this.userId});
 
   Future<void> getData() async {
-     Response response = await get(Uri.parse('http://localhost:3000/api/referral/$userId'));
+     Response response = await get(Uri.parse('http://localhost:3000/api/referral/$userId'), headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Accept': '*/*'
+    });
      List data = jsonDecode(response.body) as List;
 
      referrals = data
