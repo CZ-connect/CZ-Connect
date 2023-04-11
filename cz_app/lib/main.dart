e3df889f56f011bbc9045aed50e2ba632e8a0ad6
Marijn <marijnvandenbos59@gmail.com>
2023-04-11 12:02:01 +0200

    top-app styled

--- cz_app/lib/main.dart
+++ cz_app/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/referraldashboard': (context) => const Scaffold(
--- cz_app/lib/widget/app/referral_form/topAppLayout.dart
+++ cz_app/lib/widget/app/referral_form/topAppLayout.dart
@@ -8,10 +8,43 @@ class topAppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     //TODO make this fillable with widgets
     return Center(
-      child: Container(
-        decoration: BoxDecoration(color: color),
-        width: double.maxFinite,
-        height: 250.0,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 250,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: color,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/logo-cz.png'),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'CZ-connect',
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
