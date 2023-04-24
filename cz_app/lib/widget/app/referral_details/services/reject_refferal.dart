488dadc54ec06e3c76586e5afff7bde34363b90c
cmberge <cm.vandenberge@student.avans.nl>
2023-04-24 12:29:14 +0200

    Test error fixed

--- cz_app/lib/widget/app/referral_details/services/reject_refferal.dart
+++ cz_app/lib/widget/app/referral_details/services/reject_refferal.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class RejectReferral {
-
-  static Future<void> rejectRefferal(BuildContext context, dynamic referral) async {
+  static Future<void> rejectRefferal(
+      BuildContext context, dynamic referral) async {
     var id = referral.id.toString();
     var url = Uri.http('localhost:3000', '/api/referral/$id');
 
-    DateTime dateTime = DateFormat("yyyy-MM-dd HH:mm:ss").parse(referral.registrationDate.toString());
+    DateTime dateTime = DateFormat("yyyy-MM-dd HH:mm:ss")
+        .parse(referral.registrationDate.toString());
     String formattedDateString = dateTime.toString().replaceFirst(' ', 'T');
 
     Map<String, dynamic> jsonMap = {
@@ -25,16 +26,20 @@ class RejectReferral {
     var body = json.encode(jsonMap);
 
     try {
-      var response = await http.put(url, 
-      headers: {"Content-Type": "application/json"}, body: body);
+      var response = await http.put(url,
+          headers: {"Content-Type": "application/json"}, body: body);
 
-     if (response.statusCode >= 400 && response.statusCode <= 499) {
+      if (response.statusCode >= 400 && response.statusCode <= 499) {
+        // ignore: use_build_context_synchronously
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Client error: ${response.statusCode}')),
         );
-         throw Exception('Client error: ${response.statusCode}');
-     }
-    } catch (exception) {}
+        throw Exception('Client error: ${response.statusCode}');
+      }
+    } catch (exception) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Client error: $exception')),
+      );
+    }
   }
 }
-
--- cz_app/test/reject_refferal_test.dart
+++ cz_app/test/reject_refferal_test.dart
@@ -24,38 +24,38 @@ void main() {
     title: 'CZ_connect',
     initialRoute: '/referraldashboard',
     routes: {
-        '/referraldashboard': (context) => const Scaffold(
-              body: ReferralDashboardTemplate(
-                header: ReferralDashboardTopWidget(),
-                body: ReferralDashboardBottomWidget(
-                  child: ReferralDashboardContainerWidget(
-                    child: ReferralDashboardIndexWidget(),
-                  ),
+      '/referraldashboard': (context) => const Scaffold(
+            body: ReferralDashboardTemplate(
+              header: ReferralDashboardTopWidget(),
+              body: ReferralDashboardBottomWidget(
+                child: ReferralDashboardContainerWidget(
+                  child: ReferralDashboardIndexWidget(),
                 ),
               ),
             ),
-        '/referraldetail': (context) => const Scaffold(
-              body: ReferralDashboardTemplate(
-                header: ReferralDashboardTopWidget(),
-                body: ReferralDashboardBottomWidget(
-                  child: ReferralDashboardContainerWidget(
-                    child: ReferralDetailWidget(),
-                  ),
+          ),
+      '/referraldetail': (context) => const Scaffold(
+            body: ReferralDashboardTemplate(
+              header: ReferralDashboardTopWidget(),
+              body: ReferralDashboardBottomWidget(
+                child: ReferralDashboardContainerWidget(
+                  child: ReferralDetailWidget(),
                 ),
               ),
             ),
+          ),
     },
   );
 
-    const referral = '{"id":"1","participantName":"Coen","participantEmail":"koen@mail.com","status":"Pending","registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null}';
-   const expectedJsonResponse =
+  const referral =
+      '{"id":1,"participantName":"Coen","participantEmail":"koen@mail.com","status":"Approved","participantPhoneNumber":null,"registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null}';
+  const expectedJsonResponse =
       '{"referrals":[{"id":1,"participantName":"Coen","participantEmail":"koen@mail.com","status":"Pending","registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null}],"completed":0,"pending":1}';
   const deniedReferralJsonResponse =
       '{"referrals":[{"id":1,"participantName":"Coen","participantEmail":"koen@mail.com","status":"Denied","registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null},{"id":2,"participantName":"Koen van den Heuvel","participantEmail":"jos@exmaple.com","status":"Goedgekeurd","registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null},{"id":3,"participantName":"Koen van den Heuvel","participantEmail":"koen@mail.com","status":"Goedgekeurd","registrationDate":"2023-03-22T00:00:00","employeeId":1,"employee":null},{"id":4,"participantName":"Willem Bollekam","participantEmail":"willi@mail.com","status":"In Afwachting","registrationDate":"2023-02-08T00:00:00","employeeId":1,"employee":null},{"id":5,"participantName":"Martijn van den Woud","participantEmail":"mvdw@mail.com","status":"Afgewezen","registrationDate":"2023-01-05T00:00:00","employeeId":1,"employee":null},{"id":6,"participantName":"Marin Kieplant","participantEmail":"plantje@mail.com","status":"In Afwachting","registrationDate":"2022-08-18T00:00:00","employeeId":2,"employee":null}],"completed":3,"pending":2}';
 
   group('Reject Referral', () {
-    testWidgets('Click on a Referral succeeds',
-        (WidgetTester tester) async {
+    testWidgets('Click on a Referral succeeds', (WidgetTester tester) async {
       final interceptor = nock.get("/referral")
         ..reply(
           200,
@@ -64,11 +64,11 @@ void main() {
       nock.get("/referral").reply(
             200,
             expectedJsonResponse,
-      );
+          );
       nock.get("/referral").reply(
             200,
             expectedJsonResponse,
-      );
+          );
 
       await tester.pumpWidget(myapp);
       await tester.pumpAndSettle();
@@ -84,7 +84,7 @@ void main() {
     });
 
     testWidgets('Can not reject a already denied referral ',
-               (WidgetTester tester) async {
+        (WidgetTester tester) async {
       final interceptor = nock.get("/referral")
         ..reply(
           200,
@@ -93,11 +93,11 @@ void main() {
       nock.get("/referral").reply(
             200,
             deniedReferralJsonResponse,
-      );
+          );
       nock.get("/referral").reply(
             200,
             deniedReferralJsonResponse,
-      );
+          );
 
       await tester.runAsync(() async {
         await tester.pumpWidget(myapp);
@@ -113,11 +113,10 @@ void main() {
       expect(find.byKey(const ValueKey('referral_details')), findsOneWidget);
       expect(find.byType(ElevatedButton), findsNothing);
       expect(find.text("Afkeuren"), findsNothing);
-      
     });
 
     testWidgets('Reject Referral service succeeds ',
-               (WidgetTester tester) async {
+        (WidgetTester tester) async {
       final interceptor = nock.get("/referral")
         ..reply(
           200,
@@ -126,11 +125,11 @@ void main() {
       nock.get("/referral").reply(
             200,
             expectedJsonResponse,
-      );
+          );
       nock.get("/referral").reply(
             200,
-          expectedJsonResponse,
-      );
+            expectedJsonResponse,
+          );
       nock.put("/referral/1", referral).reply(200, referral);
 
       await tester.runAsync(() async {
@@ -143,7 +142,11 @@ void main() {
       await tester.tap(find.text("Coen"));
       await tester.pumpAndSettle();
 
-      Referral ref = Referral(id: 1, status: "Pending", participantName: "Coen", registrationDate: DateTime.parse("2023-03-22T00:00:00"));
+      Referral ref = Referral(
+          id: 1,
+          status: "Approved",
+          participantName: "Coen",
+          registrationDate: DateTime.parse("2023-03-22T00:00:00"));
 
       final BuildContext context = tester.element(find.byType(ElevatedButton));
       await RejectReferral.rejectRefferal(context, ref);
@@ -152,8 +155,8 @@ void main() {
       expect(find.byKey(const ValueKey('referral_details')), findsOneWidget);
     });
 
-      testWidgets('Reject Referral service bad request ',
-               (WidgetTester tester) async {
+    testWidgets('Reject Referral service bad request ',
+        (WidgetTester tester) async {
       final interceptor = nock.get("/referral")
         ..reply(
           200,
@@ -162,22 +165,26 @@ void main() {
       nock.get("/referral").reply(
             200,
             expectedJsonResponse,
-      );
+          );
       nock.get("/referral").reply(
             200,
-          expectedJsonResponse,
-      );
+            expectedJsonResponse,
+          );
       nock.put("/referral/1", referral).reply(400, {});
 
-    await tester.pumpWidget(myapp);
-    await tester.pumpAndSettle();
+      await tester.pumpWidget(myapp);
+      await tester.pumpAndSettle();
 
       expect(interceptor.isDone, true);
 
       await tester.tap(find.text("Coen"));
       await tester.pumpAndSettle();
 
-      Referral ref = Referral(id: 28, status: "Pending", participantName: "Coen", registrationDate: DateTime.parse("2023-03-22T00:00:00"));
+      Referral ref = Referral(
+          id: 1,
+          status: "Pending",
+          participantName: "Coen",
+          registrationDate: DateTime.parse("2023-03-22T00:00:00"));
       final BuildContext context = tester.element(find.byType(ElevatedButton));
       await RejectReferral.rejectRefferal(context, ref);
       await tester.pump();
@@ -185,6 +192,5 @@ void main() {
       expect(find.text('Client Error: 400'), findsNothing);
       expect(find.byKey(const ValueKey('referral_details')), findsOneWidget);
     });
-    
   });
 }
