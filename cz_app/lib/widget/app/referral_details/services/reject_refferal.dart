b9b8c0d8d276bf57cfef537f0d5e418377cd1dbd
JosVoogt2107 <j.voogt1@student.avans.nl>
2023-04-24 22:43:33 +0200

    aanpassingen voor bugfix
    Bugfix is nu opgelost,
    
    De refferals worden nu ook echt afgekeurd

--- cz_app/lib/widget/app/referral_details/services/reject_refferal.dart
+++ cz_app/lib/widget/app/referral_details/services/reject_refferal.dart
@@ -3,9 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class RejectReferral {
-  static Future<void> rejectRefferal(
-      BuildContext context, dynamic referral) async {
+Future<void> rejectRefferal(BuildContext context, dynamic referral) async {
     var id = referral.id.toString();
     var url = Uri.http('localhost:3000', '/api/referral/$id');
 
@@ -13,13 +11,19 @@ class RejectReferral {
         .parse(referral.registrationDate.toString());
     String formattedDateString = dateTime.toString().replaceFirst(' ', 'T');
 
+    var phone = "";
+    if(referral.participantPhoneNumber != null) {
+      phone = referral.participantPhoneNumber.toString();
+    }
+
     Map<String, dynamic> jsonMap = {
-      'id': 4,
+      'id': referral.id.toString(),
       'participantName': referral.participantName.toString(),
       'participantEmail': referral.participantEmail.toString(),
+      'participantPhoneNumber': phone,
       'status': referral.status.toString(),
       'registrationDate': formattedDateString,
-      'employeeId': 1,
+      'employeeId': referral.employeeId.toString(),
       'employee': null
     };
 
@@ -27,7 +31,7 @@ class RejectReferral {
 
     try {
       var response = await http.put(url,
-          headers: {"Content-Type": "application/json"}, body: body);
+          headers: {"Accept": "application/json","Content-Type": "application/json"}, body: body);
 
       if (response.statusCode >= 400 && response.statusCode <= 499) {
         // ignore: use_build_context_synchronously
@@ -35,11 +39,16 @@ class RejectReferral {
           SnackBar(content: Text('Client error: ${response.statusCode}')),
         );
         throw Exception('Client error: ${response.statusCode}');
-      }
+      } else if (response.statusCode >= 500 && response.statusCode <= 599) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Server error: ${response.statusCode}')),
+      );
+      throw Exception('Server error: ${response.statusCode}');
+    }
     } catch (exception) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Client error: $exception')),
+        SnackBar(content: Text('Error: $exception')),
       );
     }
-  }
 }
