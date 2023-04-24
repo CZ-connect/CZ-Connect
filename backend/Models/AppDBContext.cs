79df9800efcdc926185d02d32e5ce7efc255ffdb
Marijn <marijnvandenbos59@gmail.com>
2023-04-24 13:01:31 +0200

    added-new-migration

--- backend/Models/AppDBContext.cs
+++ backend/Models/AppDBContext.cs
@@ -5,17 +5,6 @@ namespace CZConnect.Models;
 public class AppDBContext : DbContext
 {
     public AppDBContext(DbContextOptions options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .Entity<Referral>()
-            .Property(r => r.Status)
-            .HasConversion(
-                v => v.ToString(),                
-                v => (ReferralStatus)Enum.Parse(typeof(ReferralStatus), v));
-    } 
-
     public DbSet<Referral> Referrals { get; set; }
     public DbSet<Employee> Employees { get; set;}
     public DbSet<ApplicantForm> ApplicantForms { get; set; }
@@ -36,5 +25,8 @@ public class AppDBContext : DbContext
             .HasConversion(
                 v => v.ToString(),                
                 v => (ReferralStatus)Enum.Parse(typeof(ReferralStatus), v));
-    }
+    } 
+
+  
+    
 }
