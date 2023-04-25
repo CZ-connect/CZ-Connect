95e5d968810985a1bef174e6f030179a8a58a321
cmberge <cm.vandenberge@student.avans.nl>
2023-04-25 14:44:26 +0200

    Everything works except test

--- cz_app/lib/widget/app/templates/navigation_menu.dart
+++ cz_app/lib/widget/app/templates/navigation_menu.dart
@@ -5,59 +5,64 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const SizedBox(
-              height: 100,
-              child: DrawerHeader(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Color(0xFFE40429), Color(0xFFFF9200)],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                      ),
-                  ),
-                  child: Text(
-                    'Menu',
-                    style: TextStyle(
-                      color: Colors.white
-                    ),
-                  ),
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const SizedBox(
+            height: 100,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFE40429), Color(0xFFFF9200)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
                 ),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            ListTile(
-              title: const Text('Referral Dashboard'),
-              key: const Key('referral_dashboard_menu_item'),
-              onTap: () {
-                Navigator.popAndPushNamed(context, '/referraldashboard');
-              },
-            ),
-            ListTile(
-              title: const Text('Application Form'),
-              key: const Key('application_form_menu_item'),
-              onTap: () {
-                Navigator.popAndPushNamed(context, '/');
-              },
-            ),
-            ListTile(
-              title: const Text('Graph Referals'),
-              key: const Key('Graph_refferals_menu_item'),
-              onTap: () {
-                Navigator.popAndPushNamed(context, '/graph');
-              },
-            ),
-            ListTile(
-              title: const Text('Referral Overzicht'),
-              key: const Key('referral_overview_menu_item'),
-              onTap: () {
-                Navigator.popAndPushNamed(context, '/loading');
-              },
-            ),
-          ],
-        ),
-      );
+          ),
+          ListTile(
+            title: const Text('Application Form'),
+            key: const Key('application_form_menu_item'),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/');
+            },
+          ),
+          ListTile(
+            title: const Text('Referral Dashboard'),
+            key: const Key('referral_dashboard_menu_item'),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/referraldashboard');
+            },
+          ),
+          ListTile(
+            title: const Text('Referral Overzicht'),
+            key: const Key('referral_overview_menu_item'),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/loading');
+            },
+          ),
+          ListTile(
+            title: const Text('Recruitment Overzicht'),
+            key: const Key('recruitment_overview_menu_item'),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/recruitmentdashboard');
+            },
+          ),
+          ListTile(
+            title: const Text('Graph Referals'),
+            key: const Key('Graph_refferals_menu_item'),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/graph');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
